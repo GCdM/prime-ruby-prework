5ada0a025fa4af9bf410f7f6abc13bed870893c8
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-24 18:49:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,19 @@
 # Add  code here!
 def prime?(number)
   num = number.abs
-  array = [1]
-  i = 2
-  until i > Math.sqrt(num)
-    array << i
-    i += 1
-  end
-  array.each do |x|
-    if num % x == 0
-      return false
+  array = []
+  if num <= 1
+    return false
+  else
+    i = 2
+    until i > Math.sqrt(num)
+      array << i
+      i += 1
+    end
+    array.each do |x|
+      if num % x == 0
+        return false
+      end
     end
   end
   return true
